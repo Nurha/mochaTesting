2cfa4971187f79705c64ebd0637717f6ae077bc2
nurha <you@example.com>
2017-06-07 21:27:30 +0200

    added length function and test

---
+++ js/length.js
@@ -0,0 +1,10 @@
+// takes a string parameter and
+//returns the length of it
+
+function length(string) {
+  return string.length;
+}
+
+console.log(length('I love you'));
+
+module.exports = length;
