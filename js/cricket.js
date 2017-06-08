7e1d9a1cfbe6349f48b09ea2568039fe36655a44
nurha <you@example.com>
2017-06-08 11:06:04 +0200

    working on cricket function

---
+++ js/cricket.js
@@ -0,0 +1,18 @@
+//that solves the following riddle.
+//The common field cricket chirps in direct proportion to the current tem­perature.
+//Adding 40 to the number of times a cricket chirps in a minute, then dividing by 4,
+//gives us the temperature (in Fahrenheit degrees).
+//Write a program that takes the number of cricket chirps in fifteen seconds as a parameter,
+//then outputs the current temperature to the nearest half degree.
+
+function cricket(numberOfCricketChripsInFifteenSeconds) {
+
+var numberOfChirpsInAMinute = numberOfCricketChripsInFifteenSeconds * 4;
+
+var temperatureInFarenheit = (numberOfChirpsInAMinute + 40) / 4;
+return  temperatureInFarenheit
+}
+
+console.log(cricket(30));
+
+module.exports = cricket;
