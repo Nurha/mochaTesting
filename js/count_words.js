33083c0fee6169977bf2702339ae97577924c893
nurha <nurha@projectcodex.co>
2017-06-09 12:25:29 +0200

    added count_words function

---
+++ js/count_words.js
@@ -0,0 +1,12 @@
+//takes a sentence as a parameter
+//and returns the number of words in the sentence
+
+function count_words(string) {
+
+  return string.length;
+
+}
+
+console.log(count_words('I love pudding'));
+
+module.exports = count_words;
