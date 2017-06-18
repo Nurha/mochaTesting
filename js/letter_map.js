47fc39504b7d600693ade67e76f8c8016d37153b
nurha <nurha@projectcodex.co>
2017-06-18 10:14:01 +0200

    working on letter map function

---
+++ js/letter_map.js
@@ -0,0 +1,7 @@
+// Write a function that takes a sentence as a parameter.
+// The function should return a map. 
+// The keys of the map should be the first letters from the words in the sentence. 
+// The values should be an array of any words in the sentence that start with the key of the map. 
+// eg "Let's do this today" should return {"L" : ["Let's"], d : ["do"], "t" : ["this", "today"]}.
+
+
