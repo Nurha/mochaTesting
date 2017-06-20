7770ca888b72f60cd01132cc8de82198b764ce22
nurha <nurha@projectcodex.co>
2017-06-20 08:06:57 +0200

    Refactored shortest word function

--- js/shortest_word.js
+++ js/shortest_word.js
@@ -1,16 +1,9 @@
 //takes a sentence as a parameter
 //and returns the shortest word in it, and the length of the word
 
-//split the string
-//loop though the split words
-//have a variable hold a word and compare it to a word in the sentence
-//holding the shortest word every time until it is done looping
-//once it has the shortest word find the length of that word
-
-
 function shortest_word(string) {
   var newString = string.split(' ');
-  var shortestWord = newString[0];
+  var shortestWord = newString[1];
   var wordLength = 0;
 
   var wordAndLength = [];
@@ -26,5 +19,5 @@ function shortest_word(string) {
   return wordAndLength;
 }
 
-console.log(shortest_word('ate so much cheese on the pizza'));
+console.log(shortest_word('cow co c'));
 module.exports = shortest_word;
