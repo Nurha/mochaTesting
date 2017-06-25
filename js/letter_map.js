d68a8f09812aa143822e61720cc8d5e08e3786be
nurha <nurha@projectcodex.co>
2017-06-25 21:35:07 +0200

    Letter map function trying a foreach

--- js/letter_map.js
+++ js/letter_map.js
@@ -4,42 +4,44 @@
 // The values should be an array of any words in the sentence that start with the key of the map. 
 // eg "Let's do this today" should return {"L" : ["Let's"], d : ["do"], "t" : ["this", "today"]}.
 
-function letter_map(string) {
-  var words = string.split(' ');
-  var letter = '';
-  var letters = [ ];
-  var letterObj = { };
+// function letter_map(string) {
+//   var words = string.split(' ');
+//   var letter = '';
+//   var letters = [ ];
+//   var letterObj = { };
 
-  for (i=0; i<words.length; i++) {
-    letter += words[i][0];
-    letters.push(letter[i]);
-    letterObj[letters[i]] = [ ];
+//   for (i=0; i<words.length; i++) {
+//     letter += words[i][0];
+//     letters.push(letter[i]);
+//     letterObj[letters[i]] = [ ];
+//     for (j=0; k<) {
+//       letterObj[letters[i]].push(words[i]);
+//     }
+       
     
-       letterObj[letters[i]].push(words[i]);
-    
-  }
-   console.log(letterObj);
-}
-console.log(letter_map('Let\'s do it today and then see what happens tomorrow'));
-
-module.exports = letter_map;
-
-// function letter_map(str) {
-//  var string = str.split(" ");
-//  var wordmap = {};
-//  var words = [];
-
-
-//  string.forEach(function(word){
-//    if(wordmap[word[0]] !== undefined){
-//      wordmap[word[0]].push(word);
-//    } else {
-//      words.push(word);
-//      wordmap[word[0]] = words}
-//      words = [];
-//  });
-// return wordmap
+//   }
+//    console.log(letterObj);
 // }
-// console.log(letter_map('Let\'s see what happend today, and deal with it tomorrow ??????!!!!!!!'));
+// console.log(letter_map('Let\'s do it today and then see what happens tomorrow'));
 
 // module.exports = letter_map;
+
+function letter_map(str) {
+ var string = str.split(" ");
+ var wordmap = {};
+ var words = [];
+
+
+ string.forEach(function(word){
+   if(wordmap[word[0]] !== undefined){
+     wordmap[word[0]].push(word);
+   } else {
+     words.push(word);
+     wordmap[word[0]] = words}
+     words = [];
+ });
+return wordmap
+}
+console.log(letter_map('Let\'s see what happend today, and deal with it tomorrow ??????!!!!!!!'));
+
+module.exports = letter_map;
