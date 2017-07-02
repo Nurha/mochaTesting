7b9da6f6e614a7a8469fe77ec7755e9f7d495cab
nurha <nurha@projectcodex.co>
2017-07-02 22:56:05 +0200

    working on function

---
+++ js/tri_char.js
@@ -0,0 +1,17 @@
+//takes a character as a parameter, and can draw a triangle with a base of 4 using it.
+
+function triangle(character) {
+  var shape = '';
+  var base = 4;
+  var count = 0;
+  
+  for (i=0; i<=base; i++) {
+    for (j=0; j<count; j++) {
+      shape += character;
+    };
+  };
+  return shape;
+};
+
+console.log(triangle('*'));
+module.exports = triangle;
