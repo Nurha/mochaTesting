8da73c5ad97960880e00a98b75524e4a5abfa498
nurha <nurha@projectcodex.co>
2017-07-07 09:24:50 +0200

    Working on starts function using forloops

--- js/starts.js
+++ js/starts.js
@@ -1,8 +1,21 @@
 // a sentence as a parameter and returns the letter most words starts with
 
 function starts(string) {
+  var sentenceArr = string.split(' ');
+  console.log(sentenceArr);
+  var letters = '';
+  var mostLetter = '';
 
+  for (i=0; i<sentenceArr.length; i++) {
+    letters += sentenceArr[i][0];
+    for (j=0; j<letters.length; j++) {
+      if (letters[i] === letters[i]) {
+        mostLetter.push(letters[i]);
+      }
+    }
+  }
+return letters;
 }
 
-console.log(starts());
+console.log(starts('I like bananas and I want cream cakes to'));
 module.exports = starts;
