cced8411125f32d894f41c94616a60284163f1cf
nurha <nurha@projectcodex.co>
2017-07-10 11:50:24 +0200

    Added draw function

--- js/draw.js
+++ js/draw.js
@@ -1,15 +1,20 @@
-// takes a list of strings as a parameter and prints each entry in the list to the screen
+// takes a list of strings as a parameter 
+//and prints each entry in the list to the screen
 
 function draw(stringOne, stringTwo, stringThree, stringFour) {
   var arrayList = [];
+  var string = ''; 
 
   arrayList.push(stringOne);
   arrayList.push(stringTwo);
   arrayList.push(stringThree);
   arrayList.push(stringFour);
-
   
-  return arrayList;
+
+  for (i=0; i<arrayList.length; i++) { 
+    string += arrayList[i];
+  }
+  return string;
 }
 console.log(draw('*', '*', '*', '*'));
 module.exports = draw;
