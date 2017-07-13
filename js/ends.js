48a11aa0d1e56fcc424bfbafd29cd720fd228f19
nurha <nurha@projectcodex.co>
2017-07-13 23:51:14 +0200

    Working on ends function

--- js/ends.js
+++ js/ends.js
@@ -3,9 +3,33 @@
 
 function ends(string) {
   var splitString = string.split(' ');
-  console.log(splitString)
+  //console.log(splitString)
+  var map = {};
+  var maxLength = 0;
+  var occursMost = '';
 
+  splitString.forEach(function(word) {
+    //console.log(word);
+    var lastLetters = word[word.length - 1];
+    //console.log(lastLetters)
+    if (map[lastLetters]) {
+      map[lastLetters].push(word);
+    } else {
+      map[lastLetters] = []
+      map[lastLetters].push(word);
+    }
+  });
+  console.log(map)
+
+  Object.values(map).forEach(function(array) {
+    // console.log(array)
+    if (array.length > maxLength) {
+      maxLength = array.length;
+      occursMost = array[array.length - 1].toLowerCase();
+    }
+  });
+  return occursMost;
 }
 
-console.log(ends('Hello world again, but it\'s the end of the day'));
+console.log(ends('hed end of the dayd'));
 module.exports = ends;
