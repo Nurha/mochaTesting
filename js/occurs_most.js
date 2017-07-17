472aa8e480590c3c5190a72320859626844b3f15
nurha <nurha@projectcodex.co>
2017-07-17 19:07:52 +0200

    Added occurs most with for each

--- js/occurs_most.js
+++ js/occurs_most.js
@@ -5,13 +5,28 @@ function occurs_most(string) {
   string = string.replace(/\s+/g, '');
   var splitString = string.split('');
   var count = 0;
+  var letterMap = {};
+  var occursMost = '';
 
-  splitString.forEach(function(letter) {
-    console.log(letter);
-    if (letter === splitString) {
-      count++;
-    }
+  splitString.forEach(function(letterA) {
+    splitString.forEach(function(letter) {
+      if (letterA === letter) {
+        count++;
+        letterMap[letterA] = count;
+      };
+    });
+    count = 0;
   });
+
+  var max = 0;
+  for (x in letterMap) {
+    if (letterMap[x] > max) {
+      max = letterMap[x];
+      occursMost = x;
+    }
+  }
+  return occursMost;
+  console.log(occursMost);
 }
-console.log(occurs_most('this letter occurs the  most in this string'));
+console.log(occurs_most('this letter occurs the most in this strinp'));
 module.exports = occurs_most;
