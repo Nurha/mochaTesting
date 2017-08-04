48b2165942e4c259e36b20d00f1557f5f4f4713f
Nurha <nurha@projectcodex.co>
2017-08-04 15:47:59 +0200

    Refactored high low function

--- js/high_low.js
+++ js/high_low.js
@@ -1,23 +1,40 @@
 //takes a list of numbers as a parameter
 //and returns the highest and lowest numbers in the list
 
-function high_low(numberOne, numberTwo, numberThree, numberFour) {
+// function high_low(numberOne, numberTwo, numberThree, numberFour) {
 
-  var numberList = [];
-  numberList.push(numberOne, numberTwo, numberThree, numberFour);
+//   var numberList = [];
+//   numberList.push(numberOne, numberTwo, numberThree, numberFour);
 
-  var highestNumber = Math.max(...numberList);
+//   var highestNumber = Math.max(...numberList);
 
-  var lowestNumber = Math.min(...numberList);
+//   var lowestNumber = Math.min(...numberList);
 
-  var highestNumberAndLowestNumber = [];
+//   var highestNumberAndLowestNumber = [];
 
-  highestNumberAndLowestNumber.push(highestNumber);
-  highestNumberAndLowestNumber.push(lowestNumber);
+//   highestNumberAndLowestNumber.push(highestNumber);
+//   highestNumberAndLowestNumber.push(lowestNumber);
 
-  return highestNumberAndLowestNumber;
+//   return highestNumberAndLowestNumber;
+// }
+
+// console.log(high_low(15, 1, 9, 4));
+
+// module.exports = high_low;
+
+function high_low(numOne, numTwo, numThree, numFour) {
+  var higestNumber = Math.max(numOne, numTwo, numThree, numFour);
+  console.log(higestNumber);
+
+  var lowestNumber = Math.min(numOne, numTwo, numThree, numFour);
+  console.log(lowestNumber);
+
+  var highLow = [];
+  highLow.push(higestNumber);
+  highLow.push(lowestNumber);
+
+  return highLow;
+
 }
-
-console.log(high_low(15, 1, 9, 4));
-
+console.log(high_low(1,9,5,7));
 module.exports = high_low;
